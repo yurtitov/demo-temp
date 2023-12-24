6358dc2deb91cd44f77e258552df97b34bb40093
Yuriy Titov <yurtitov@gmail.com>
2023-12-24 13:45:15 +0200

    wip refactoring

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,52 +1,44 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.projectFeatures.githubConnection
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2023.11"
 
 project {
 
-    buildType(Build)
-    buildType(FastTest)
-    buildType(SlowTest)
-    buildType(Package)
 
-    sequential {
-        buildType(Build)
+    val buildTypes = sequential {
+        buildType(Maven("Build", "clean compile"))
         parallel {
-            buildType(FastTest)
-            buildType(SlowTest)
+            buildType(
+                Maven(
+                    "Unit Tests",
+                    "clean test",
+                    "-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test"
+                )
+            )
+            buildType(
+                Maven(
+                    "Integration Tests",
+                    "clean test",
+                    "-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test"
+                )
+            )
         }
-        buildType(Package)
+        buildType(Maven("Package", "clean package"))
+    }.buildTypes()
+
+    buildTypes.forEach(::buildType)
+    buildTypes.last().triggers {
+        vcs {
+
+        }
     }
 }
 
-object Build : BuildType({
-    name = "Build"
+class Maven(name: String, goals: String, runnerArgs: String? = null) : BuildType({
+    this.name = name
 
     vcs {
         root(DslContext.settingsRoot)
@@ -54,77 +46,8 @@ object Build : BuildType({
 
     steps {
         maven {
-            name = "Compile step"
-            goals = "clean compile"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            jdkHome = "%env.JDK_17_0%"
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-})
-
-object FastTest : BuildType({
-    name = "FastTest"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test"
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-})
-
-object SlowTest : BuildType({
-    name = "SlowTest"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test"
-        }
-    }
-
-    features {
-        perfmon {
-        }
-    }
-})
-
-object Package : BuildType({
-    name = "Package"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            name = "Package step"
-            goals = "clean package"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            jdkHome = "%env.JDK_17_0%"
-        }
-    }
-
-    triggers {
-        vcs {
+            this.goals = goals
+            this.runnerArgs = runnerArgs
         }
     }
 
