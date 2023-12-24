d27cdbbd301a8ffcb0c67b5d5559434b760b1292
Yuriy Titov <yurtitov@gmail.com>
2023-12-24 19:50:10 +0200

    wip

--- .teamcity/scripts/prepare.sh
+++ .teamcity/scripts/prepare.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir app_sources
-ls -al
-ls -al scripts
-cp scripts/launch.sh app_sources/
+cp .teamcity/scripts/launch.sh app_sources/
 cp target/demo-*.jar app_sources/app.jar
